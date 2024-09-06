52c40e8ab473f45458092fac8a14117d15542490
Diego Gajardo <deskodoo@a2group.cl>
2024-09-06 12:52:17 -0400

    33

--- account_payment_flow/__manifest__.py
+++ account_payment_flow/__manifest__.py
@@ -1,11 +1,15 @@
-
 {
     'name': "Account Payment Flow",
     'summary': "Manage payment flows",
     'author': "MP Flow",
     'website': "https://example.com",
     'category': 'Accounting',
-    'version': '17.0.0.0.1',
+    'version': '17.0',
     'depends': ['account'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/account_payment_flow_view.xml',
+        'views/account_payment_regiter_views.xml',
+    ],
     'license': 'AGPL-3',
 }
