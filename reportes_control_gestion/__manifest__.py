5bc6e63e152a82a541bf74f8b198b69cac9fb810
Diego Gajardo <deskodoo@a2group.cl>
2024-09-11 14:29:18 -0300

    new menu

---
+++ reportes_control_gestion/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Reportes Control de Gestión',
+    'version': '1.0',
+    'category': 'Reporting',
+    'summary': 'Módulo que agrega un menú principal para Reportes de Control de Gestión',
+    'author': 'Tu Nombre',
+    'website': 'http://www.tuweb.com',
+    'depends': ['base'],
+    'data': [
+        'views/menus.xml',
+    ],
+    'installable': True,
+    'application': True,
+    'license': 'LGPL-3',
+}
