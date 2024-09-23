9f9d576d12d4104ec5202e11d5a494d239ff986f
Diego Gajardo <deskodoo@a2group.cl>
2024-09-23 13:52:57 -0300

    varios

---
+++ account_payment_flujo_extension/models/account_move.py
@@ -0,0 +1,13 @@
+from odoo import models, fields
+
+class AccountMove(models.Model):
+    _inherit = 'account.move'
+
+    mp_flujo_id = fields.Many2one('mp.flujo', string="Flujo")
+    mp_grupo_flujo_id = fields.Many2one('mp.grupo.flujo', string="Grupo de Flujo")
+
+class AccountMoveLine(models.Model):
+    _inherit = 'account.move.line'
+
+    mp_flujo_id = fields.Many2one(related='move_id.mp_flujo_id', store=True, string="Flujo")
+    mp_grupo_flujo_id = fields.Many2one(related='move_id.mp_grupo_flujo_id', store=True, string="Grupo de Flujo")
--- account_payment_flujo_extension/models/account_payment.py
+++ account_payment_flujo_extension/models/account_payment.py
@@ -5,3 +5,28 @@ class AccountPayment(models.Model):
 
     mpflujo = fields.Many2one('mp.flujo', string='Flujo')
     mpgrupo_flujo = fields.Many2one('mp.grupo.flujo', string='Grupo de Flujo')
+
+
+    def _create_payment_entry(self, amount):
+        move = super(AccountPayment, self)._create_payment_entry(amount)
+
+        # Heredar los campos `mp_flujo_id` y `mp_grupo_flujo_id` al asiento contable
+        move.update({
+            'mp_flujo_id': self.mp_flujo_id.id,
+            'mp_grupo_flujo_id': self.mp_grupo_flujo_id.id
+        })
+
+        return move
+
+
+class AccountPaymentRegister(models.TransientModel):
+    _inherit = 'account.payment.register'
+
+    def _create_payments(self, to_process):
+        payments = super(AccountPaymentRegister, self)._create_payments(to_process)
+
+        for payment in payments:
+            payment.mp_flujo_id = self.mp_flujo_id
+            payment.mp_grupo_flujo_id = self.mp_grupo_flujo_id
+
+        return payments
