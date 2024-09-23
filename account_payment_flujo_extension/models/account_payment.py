6b8f071f13301edaa2cfc5cc33571c5593699a27
Diego Gajardo <deskodoo@a2group.cl>
2024-09-23 16:32:32 -0300

    action_post 2

--- account_payment_flujo_extension/models/account_payment.py
+++ account_payment_flujo_extension/models/account_payment.py
@@ -7,25 +7,19 @@ class AccountPayment(models.Model):
     mpgrupo_flujo = fields.Many2one('mp.grupo.flujo', string='Grupo de Flujo')
 
     def action_post(self):
-        # Llamar a la función original para validar el pago
+        # Llamamos a la función original para que continúe con el flujo de generación de asiento contable
         res = super(AccountPayment, self).action_post()
 
+        # Iteramos sobre los pagos para heredar los valores de flujo y grupo de flujo a los asientos contables
         for payment in self:
-            # Buscar el asiento contable generado por el pago
-            account_move = payment.move_id
-
-            if account_move:
-                # Asignar los valores de flujo y grupo de flujo al asiento contable
-                account_move.sudo().write({
-                    'mp_flujo_id': payment.mp_flujo_id.id,
-                    'mp_grupo_flujo_id': payment.mp_grupo_flujo_id.id
-                })
-
-                # Asignar también a las líneas del asiento contable
-                for line in account_move.line_ids:
-                    line.sudo().write({
-                        'mp_flujo_id': payment.mp_flujo_id.id,
-                        'mp_grupo_flujo_id': payment.mp_grupo_flujo_id.id
-                    })
+            if payment.move_id:
+                # Añadimos los valores del flujo y grupo de flujo al asiento contable generado
+                payment.move_id.mp_flujo_id = payment.mp_flujo_id
+                payment.move_id.mp_grupo_flujo_id = payment.mp_grupo_flujo_id
+                
+                # Ahora heredamos los valores de flujo y grupo de flujo también a las líneas del asiento
+                for move_line in payment.move_id.line_ids:
+                    move_line.mp_flujo_id = payment.mp_flujo_id
+                    move_line.mp_grupo_flujo_id = payment.mp_grupo_flujo_id
 
         return res
