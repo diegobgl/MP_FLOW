0f3e8d1ab952a623295be6ae5ab5305eccedc763
Diego Gajardo <deskodoo@a2group.cl>
2024-09-23 17:06:31 -0300

    new method

--- account_payment_flujo_extension/models/account_move.py
+++ account_payment_flujo_extension/models/account_move.py
@@ -6,8 +6,31 @@ class AccountMove(models.Model):
     mp_flujo_id = fields.Many2one('mp.flujo', string="Flujo")
     mp_grupo_flujo_id = fields.Many2one('mp.grupo.flujo', string="Grupo de Flujo")
 
+    def _post(self, soft=True):
+        res = super(AccountMove, self)._post(soft)
+        
+        # Assign 'Flujo' and 'Grupo de Flujo' to journal entries
+        for move in self:
+            if move.payment_id:
+                move.write({
+                    'mp_flujo_id': move.payment_id.mp_flujo_id.id,
+                    'mp_grupo_flujo_id': move.payment_id.mp_grupo_flujo_id.id,
+                })
+                
+                # Also update the lines
+                for line in move.line_ids:
+                    line.write({
+                        'mp_flujo_id': move.payment_id.mp_flujo_id.id,
+                        'mp_grupo_flujo_id': move.payment_id.mp_grupo_flujo_id.id,
+                    })
+
+        return res
+
+
 class AccountMoveLine(models.Model):
     _inherit = 'account.move.line'
 
     mp_flujo_id = fields.Many2one(related='move_id.mp_flujo_id', store=True, string="Flujo")
     mp_grupo_flujo_id = fields.Many2one(related='move_id.mp_grupo_flujo_id', store=True, string="Grupo de Flujo")
+
+
--- account_payment_flujo_extension/models/account_payment.py
+++ account_payment_flujo_extension/models/account_payment.py
@@ -6,28 +6,23 @@ class AccountPayment(models.Model):
     mpflujo = fields.Many2one('mp.flujo', string='Flujo')
     mpgrupo_flujo = fields.Many2one('mp.grupo.flujo', string='Grupo de Flujo')
 
-    def action_post(self):
-        # First, execute the standard method to create the payment and journal entry
-        res = super(AccountPayment, self).action_post()
+    def _create_payment_entry(self, amount):
+        # Call the super method to create the journal entry
+        move = super(AccountPayment, self)._create_payment_entry(amount)
+        
+        # Assign the 'Flujo' and 'Grupo de Flujo' from the payment to the account.move
+        if self.mp_flujo_id or self.mp_grupo_flujo_id:
+            move.write({
+                'mp_flujo_id': self.mp_flujo_id.id,
+                'mp_grupo_flujo_id': self.mp_grupo_flujo_id.id,
+            })
 
-        # Loop through the payments to find the related journal entries
-        for payment in self:
-            # Get the related journal entries (account.move)
-            account_move = payment.move_id  # Assuming `move_id` links to the journal entry
+        # Assign the 'Flujo' and 'Grupo de Flujo' to the lines of the journal entry
+        for line in move.line_ids:
+            line.write({
+                'mp_flujo_id': self.mp_flujo_id.id,
+                'mp_grupo_flujo_id': self.mp_grupo_flujo_id.id,
+            })
 
-            if account_move:
-                # Assign the "Flujo" and "Grupo de Flujo" from the payment to the journal entry
-                account_move.write({
-                    'mp_flujo_id': payment.mp_flujo_id.id,
-                    'mp_grupo_flujo_id': payment.mp_grupo_flujo_id.id,
-                })
-
-                # Assign the "Flujo" and "Grupo de Flujo" to the lines of the journal entry
-                for line in account_move.line_ids:
-                    line.write({
-                        'mp_flujo_id': payment.mp_flujo_id.id,
-                        'mp_grupo_flujo_id': payment.mp_grupo_flujo_id.id,
-                    })
-
-        return res
+        return move
 
