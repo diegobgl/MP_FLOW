5134af35262e78e28d83b18add66a1bd4255ea86
Diego Gajardo <deskodoo@a2group.cl>
2024-09-23 14:40:33 -0300

    funciones

--- account_payment_flujo_extension/models/account_payment.py
+++ account_payment_flujo_extension/models/account_payment.py
@@ -6,27 +6,18 @@ class AccountPayment(models.Model):
     mpflujo = fields.Many2one('mp.flujo', string='Flujo')
     mpgrupo_flujo = fields.Many2one('mp.grupo.flujo', string='Grupo de Flujo')
 
-
+    @api.model
     def _create_payment_entry(self, amount):
+        # Llamada al super para generar el asiento contable
         move = super(AccountPayment, self)._create_payment_entry(amount)
-
-        # Heredar los campos `mp_flujo_id` y `mp_grupo_flujo_id` al asiento contable
-        move.update({
-            'mp_flujo_id': self.mp_flujo_id.id,
-            'mp_grupo_flujo_id': self.mp_grupo_flujo_id.id
-        })
+        
+        # Asignar los valores de flujo y grupo de flujo al asiento contable
+        move.mp_flujo_id = self.mp_flujo_id
+        move.mp_grupo_flujo_id = self.mp_grupo_flujo_id
+        
+        # Asignar también a las líneas del asiento si es necesario
+        for line in move.line_ids:
+            line.mp_flujo_id = self.mp_flujo_id
+            line.mp_grupo_flujo_id = self.mp_grupo_flujo_id
 
         return move
-
-
-class AccountPaymentRegister(models.TransientModel):
-    _inherit = 'account.payment.register'
-
-    def _create_payments(self, to_process):
-        payments = super(AccountPaymentRegister, self)._create_payments(to_process)
-
-        for payment in payments:
-            payment.mp_flujo_id = self.mp_flujo_id
-            payment.mp_grupo_flujo_id = self.mp_grupo_flujo_id
-
-        return payments
