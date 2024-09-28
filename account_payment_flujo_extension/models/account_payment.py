195f3b452b79fd90b1ea765973c33890c7d1cd58
Diego Gajardo <deskodoo@a2group.cl>
2024-09-28 13:41:37 -0300

    new version

--- account_payment_flujo_extension/models/account_payment.py
+++ account_payment_flujo_extension/models/account_payment.py
@@ -1,9 +1,9 @@
 from odoo import models, fields, api, _
-from odoo.exceptions import ValidationError  # Importar ValidationError
 import logging
 
 _logger = logging.getLogger(__name__)
 
+# Modelo account.payment
 class AccountPayment(models.Model):
     _inherit = 'account.payment'
 
@@ -12,31 +12,52 @@ class AccountPayment(models.Model):
 
     @api.model
     def create(self, vals_list):
-        """Sobrescribe el método create para asegurarse que Flujo y Grupo de Flujo se asignen correctamente.
-        Soporta tanto pagos individuales como múltiples.
-        """
-        return super(AccountPayment, self).create(vals_list)
+        """Sobrescribe el método create para asegurarse que Flujo y Grupo de Flujo se asignen correctamente."""
+        _logger.info('Creando pago con valores: %s', vals_list)
+
+        # Asegurarse de que los valores de flujo se están pasando
+        if not vals_list.get('mp_flujo_id') and self.env.context.get('default_mp_flujo_id'):
+            vals_list['mp_flujo_id'] = self.env.context.get('default_mp_flujo_id')
+        if not vals_list.get('mp_grupo_flujo_id') and self.env.context.get('default_mp_grupo_flujo_id'):
+            vals_list['mp_grupo_flujo_id'] = self.env.context.get('default_mp_grupo_flujo_id')
+
+        payment = super(AccountPayment, self).create(vals_list)
+
+        # Asignar los valores de Flujo y Grupo de Flujo al asiento contable (account.move)
+        if payment.move_id:
+            _logger.info('Asignando Flujo y Grupo de Flujo al asiento contable (account.move) del pago %s', payment.id)
+            payment.move_id.sudo().write({
+                'mp_flujo_id': payment.mp_flujo_id.id,
+                'mp_grupo_flujo_id': payment.mp_grupo_flujo_id.id
+            })
+
+            # Asignar a las líneas del asiento contable
+            for line in payment.move_id.line_ids:
+                line.sudo().write({
+                    'mp_flujo_id': payment.mp_flujo_id.id,
+                    'mp_grupo_flujo_id': payment.mp_grupo_flujo_id.id
+                })
+
+        return payment
 
     def write(self, vals):
-        """Sobrescribe el método write para asegurarse que Flujo y Grupo de Flujo se escriban correctamente"""
+        """Sobreescribe el método write para asegurarse que Flujo y Grupo de Flujo se escriban correctamente."""
         _logger.info("Escribiendo valores de Flujo y Grupo de Flujo: %s", vals)
         return super(AccountPayment, self).write(vals)
 
     def action_post(self):
         """Sobreescribe el método action_post para asignar los valores de Flujo y Grupo de Flujo a los asientos contables."""
-        # Llamada al método original de Odoo para procesar el pago
         res = super(AccountPayment, self).action_post()
 
-        # Asignar los valores de Flujo y Grupo de Flujo a los asientos contables y sus líneas
         for payment in self:
-            if payment.move_id:  # Asegúrate de que el asiento contable (move_id) existe
+            if payment.move_id:  # Verificamos si existe el asiento contable (move_id)
                 _logger.info("Asignando Flujo y Grupo de Flujo al asiento contable (account.move) del pago: %s", payment.id)
                 payment.move_id.sudo().write({
                     'mp_flujo_id': payment.mp_flujo_id.id,
                     'mp_grupo_flujo_id': payment.mp_grupo_flujo_id.id
                 })
-                
-                # Ahora también asignamos a las líneas del asiento contable (account.move.line)
+
+                # Asignar también a las líneas del asiento contable
                 for move_line in payment.move_id.line_ids:
                     _logger.info("Asignando Flujo y Grupo de Flujo a las líneas del asiento contable (account.move.line): %s", move_line.id)
                     move_line.sudo().write({
@@ -49,27 +70,33 @@ class AccountPayment(models.Model):
         return res
 
 
-
-_logger = logging.getLogger(__name__)
-
+# Wizard account.payment.register
 class AccountPaymentRegister(models.TransientModel):
     _inherit = 'account.payment.register'
 
-    mp_flujo_id = fields.Many2one(comodel_name="mp.flujo", string="Flujo")
-    mp_grupo_flujo_id = fields.Many2one('mp.grupo.flujo', string="Grupo de Flujo")
+    mp_flujo_id = fields.Many2one(comodel_name="mp.flujo", string="Flujo", required=True)
+    mp_grupo_flujo_id = fields.Many2one('mp.grupo.flujo', string="Grupo de Flujo", required=True)
 
-   
     @api.model
     def default_get(self, fields_list):
-        """
-        Asegurarnos de que los valores de Flujo y Grupo de Flujo se incluyan cuando se crea el registro del wizard.
-        """
+        """Asegurarnos de que los valores de Flujo y Grupo de Flujo se incluyan cuando se crea el registro del wizard."""
         res = super(AccountPaymentRegister, self).default_get(fields_list)
         if 'mp_flujo_id' in self._context:
             res['mp_flujo_id'] = self._context.get('mp_flujo_id')
         if 'mp_grupo_flujo_id' in self._context:
             res['mp_grupo_flujo_id'] = self._context.get('mp_grupo_flujo_id')
         return res
+
+    def _create_payment_vals_from_wizard(self, batch_result):
+        """
+        Sobrescribe la función para asegurarse de que Flujo y Grupo de Flujo se asignen correctamente al crear los pagos.
+        """
+        payment_vals = super(AccountPaymentRegister, self)._create_payment_vals_from_wizard(batch_result)
+        payment_vals.update({
+            'mp_flujo_id': self.mp_flujo_id.id,
+            'mp_grupo_flujo_id': self.mp_grupo_flujo_id.id,
+        })
+        return payment_vals
 
     def action_create_payments(self):
         """
@@ -85,7 +112,7 @@ class AccountPaymentRegister(models.TransientModel):
             return action
 
         # Si se trata de registros de pagos, iteramos y asignamos los valores
-        payments = action  # Esto debería ser un conjunto de registros de pagos
+        payments = action
         for payment in payments:
             # Asignar Flujo y Grupo de Flujo al pago
             payment.write({
@@ -118,4 +145,3 @@ class AccountPaymentRegister(models.TransientModel):
                 _logger.warning("No se encontraron asientos contables asociados al pago %s", payment.id)
 
         return action
-
