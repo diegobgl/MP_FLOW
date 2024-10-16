7317ce7de72124db21d215bdace76ac15b0bd8e5
Diego Gajardo <deskodoo@a2group.cl>
2024-10-16 16:28:24 -0300

    new version

--- account_payment_flow/models/mp_flujo.py
+++ account_payment_flow/models/mp_flujo.py
@@ -6,7 +6,7 @@ class MpFlujo(models.Model):
 
     codigo = fields.Char(string="Código")
     
-    # Define el Many2many con un nombre de relación único
+    # Define la relación Many2many simplificada
     grupo_flujo_ids = fields.Many2many(
         comodel_name="mp.grupo.flujo",
         relation="mp_flujo_grupo_rel",  # Nombre de relación único
@@ -15,19 +15,14 @@ class MpFlujo(models.Model):
         string="Grupos de Flujo"
     )
 
-    decripcion = fields.Text(string="Descripción")
+    descripcion = fields.Text(string="Descripción")
     display_name = fields.Char(
         compute="_compute_display_name", 
         string="Nombre de Visualización", 
         store=True
     )
-    
-    mp_grupo_flujo_id = fields.Many2one(
-        comodel_name="mp.grupo.flujo", 
-        string="Grupo de Flujo"
-    )
 
-    @api.depends('codigo', 'decripcion')
+    @api.depends('codigo', 'descripcion')
     def _compute_display_name(self):
         for flujo in self:
-            flujo.display_name = f"{flujo.codigo or ''}: {flujo.decripcion or ''}"
+            flujo.display_name = f"{flujo.codigo or ''}: {flujo.descripcion or ''}"
--- account_payment_flow/models/mp_grupo_flujo.py
+++ account_payment_flow/models/mp_grupo_flujo.py
@@ -5,27 +5,11 @@ class MpGrupoFlujo(models.Model):
     _rec_name = "nombre"
 
     nombre = fields.Char(string="Nombre")
-    flujo_id = fields.Many2one(comodel_name="mp.flujo", string="Flujo")
 
-    # Ajustando la relación para que sea única respecto a otros campos Many2many
-    grupo_flujo_ids = fields.Many2many(
-        comodel_name="mp.flujo",
-        relation="mp_grupo_flujo_grupo_rel",  # Usa un nombre de relación diferente para evitar conflictos
-        column1="grupo_flujo_id",
-        column2="flujo_id",
-        string="Grupos de Flujo"
-    )
-    
-    mp_flujo_ids = fields.One2many(
-        comodel_name="mp.flujo",
-        inverse_name="mp_grupo_flujo_id",
-        string="Flujos Relacionados"
-    )
-
-    # Otra relación Many2many con un nombre de tabla único
+    # Relación simplificada para conectar con los flujos
     flujo_ids = fields.Many2many(
         comodel_name="mp.flujo",
-        relation="mp_grupo_flujo_flujo_rel",  # Usa un nombre de relación único
+        relation="mp_flujo_grupo_rel",  # Reutilizar la misma relación
         column1="grupo_flujo_id",
         column2="flujo_id",
         string="Flujos"
